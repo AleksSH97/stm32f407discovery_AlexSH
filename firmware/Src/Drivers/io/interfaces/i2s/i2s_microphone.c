45c12d287c1fb43ea37d866ac92dce3f047f716f
Alexandr Shabalin <shabalin@voltsbattery.com>
2023-02-05 16:54:50 +0400

    fw: drivers: interfaces: fix dma init
    description: change some parameters of dma init, add NVIC functions to enable interrupts of dma1_stream3 (i2s2 rx dma)

--- firmware/Src/Drivers/io/interfaces/i2s/i2s_microphone.c
+++ firmware/Src/Drivers/io/interfaces/i2s/i2s_microphone.c
@@ -27,8 +27,7 @@ void i2s2_msp_init(void)
     PeriphClkInitStruct.PLLI2S.PLLI2SN       = 192;
     PeriphClkInitStruct.PLLI2S.PLLI2SR       = 2;
 
-    if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK)
-    {
+    if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK) {
       Error_Handler();
     }
 
@@ -65,30 +64,35 @@ void i2s2_msp_init(void)
     hdma_spi2_rx.Init.MemInc              = DMA_MINC_ENABLE;
     hdma_spi2_rx.Init.PeriphDataAlignment = DMA_PDATAALIGN_HALFWORD;
     hdma_spi2_rx.Init.MemDataAlignment    = DMA_MDATAALIGN_HALFWORD;
-    hdma_spi2_rx.Init.Mode                = DMA_NORMAL;
-    hdma_spi2_rx.Init.Priority            = DMA_PRIORITY_LOW;
+    hdma_spi2_rx.Init.Mode                = DMA_CIRCULAR;
+    hdma_spi2_rx.Init.Priority            = DMA_PRIORITY_HIGH;
     hdma_spi2_rx.Init.FIFOMode            = DMA_FIFOMODE_DISABLE;
+    hdma_spi2_rx.Init.FIFOThreshold       = DMA_FIFO_THRESHOLD_FULL;
+    hdma_spi2_rx.Init.MemBurst            = DMA_MBURST_SINGLE;
+    hdma_spi2_rx.Init.PeriphBurst         = DMA_MBURST_SINGLE;
 
-    if (HAL_DMA_Init(&hdma_spi2_rx) != HAL_OK)
-    {
+    if (HAL_DMA_Init(&hdma_spi2_rx) != HAL_OK) {
       Error_Handler();
     }
 
     __HAL_LINKDMA(&hi2s2,hdmarx,hdma_spi2_rx);
+
+    HAL_NVIC_SetPriority(DMA1_Stream3_IRQn, 6, 0);
+    HAL_NVIC_EnableIRQ(DMA1_Stream3_IRQn);
 }
 
 void i2s2_init(void)
 {
     __HAL_I2S_DISABLE(&hi2s2);
 
-    hi2s2.Instance = SPI2;
-    hi2s2.Init.Mode = I2S_MODE_SLAVE_RX;
-    hi2s2.Init.Standard = I2S_STANDARD_PHILIPS;
-    hi2s2.Init.DataFormat = I2S_DATAFORMAT_16B;
-    hi2s2.Init.MCLKOutput = I2S_MCLKOUTPUT_DISABLE;
-    hi2s2.Init.AudioFreq = I2S_AUDIOFREQ_48K;
-    hi2s2.Init.CPOL = I2S_CPOL_HIGH;
-    hi2s2.Init.ClockSource = I2S_CLOCK_PLL;
+    hi2s2.Instance            = SPI2;
+    hi2s2.Init.Mode           = I2S_MODE_SLAVE_RX;
+    hi2s2.Init.Standard       = I2S_STANDARD_PHILIPS;
+    hi2s2.Init.DataFormat     = I2S_DATAFORMAT_16B;
+    hi2s2.Init.MCLKOutput     = I2S_MCLKOUTPUT_DISABLE;
+    hi2s2.Init.AudioFreq      = I2S_AUDIOFREQ_48K;
+    hi2s2.Init.CPOL           = I2S_CPOL_HIGH;
+    hi2s2.Init.ClockSource    = I2S_CLOCK_PLL;
     hi2s2.Init.FullDuplexMode = I2S_FULLDUPLEXMODE_DISABLE;
 
     if (HAL_I2S_Init(&hi2s2) != HAL_OK) {
@@ -98,7 +102,10 @@ void i2s2_init(void)
     i2s2_msp_init();
 }
 
-
+void HAL_I2S_RxHalfCpltCallback(I2S_HandleTypeDef *hi2s)
+{
+    microphone_half_transfer_callback(hi2s);
+}
 
 
 
