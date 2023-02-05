8b7c09479dafed411204db0d3bce1ef754bd615b
Alexandr Shabalin <shabalin@voltsbattery.com>
2023-02-05 16:12:18 +0400

    fw: interfaces: start to make microphone driver
    description: implement low level i2s2 initialization func with dma. first init of i2s2 was without any errors

---
+++ firmware/Inc/io/interfaces/i2s/i2s_microphone.h
@@ -0,0 +1,14 @@
+/*
+ * i2s_microphone.h
+ *
+ *  Created on: 5 февр. 2023 г.
+ *      Author: АлексанDOOR
+ */
+
+
+#include "microphone.h"
+#include "stm32f4xx_hal.h"
+
+
+
+void i2s_microphone_init(void);
