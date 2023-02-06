846f1e01982369d6e212b12f382ff6324f67d7f7
Alexandr Shabalin <shabalin@voltsbattery.com>
2023-02-06 22:43:26 +0400

    fw: interfaces: i2s_microphone: add dma receive fn
    description: add receive dma func which gets bytes and writes them to buffer. Callbacks will write certain amount of bytes of ring buffer

--- firmware/Inc/io/interfaces/i2s/i2s_microphone.h
+++ firmware/Inc/io/interfaces/i2s/i2s_microphone.h
@@ -5,8 +5,20 @@
  *      Author: АлексанDOOR
  */
 
+#ifndef I2S_MICROPHONE_H_
+#define I2S_MICROPHONE_H_
+
+#include <stdint.h>
+#include <stdbool.h>
 
 #include "microphone.h"
 #include "stm32f4xx_hal.h"
 
+#define I2S_MICROPHONE_PLLI2SN         192
+#define I2S_MICROPHONE_PLLI2SR         2
+#define I2S_MICROPHONE_PERIPH_CLOCK    RCC_PERIPHCLK_I2S
+
 void i2s_microphone_init(void);
+void i2s_setup_receive_dma(void);
+
+#endif /* I2S_MICROPHONE_H_ */
