299fcfc76884d32d7cd8ec2844c5973d58e48561
Alexandr Shabalin <shabalin@voltsbattery.com>
2023-04-12 16:05:42 +0300

    fw: drivers: io: uart: data_uart: change PIN
    description: change GPIO10C to GPIO8D because of I2S3 for DAC

--- firmware/Src/Drivers/io/interfaces/uart/data_uart.c
+++ firmware/Src/Drivers/io/interfaces/uart/data_uart.c
@@ -26,12 +26,12 @@ void DataUartInit(void)
     GPIO_InitStruct.Alternate    = GPIO_AF7_USART3;
     HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-    GPIO_InitStruct.Pin          = GPIO_PIN_10;
+    GPIO_InitStruct.Pin          = GPIO_PIN_8;
     GPIO_InitStruct.Mode         = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull         = GPIO_NOPULL;
     GPIO_InitStruct.Speed        = GPIO_SPEED_FREQ_VERY_HIGH;
     GPIO_InitStruct.Alternate    = GPIO_AF7_USART3;
-    HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+    HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
 
     __HAL_RCC_USART3_CLK_ENABLE();
 
