47ccbd9bc4c973256171a015ef225666286a740b
Alexandr Shabalin <shabalin@voltsbattery.com>
2023-04-12 18:22:20 +0300

    fw: hal_msp: add I2C init
    description: low level I2C init function

--- firmware/Src/stm32f4xx_hal_msp.c
+++ firmware/Src/stm32f4xx_hal_msp.c
@@ -128,6 +128,36 @@ void HAL_I2S_MspInit(I2S_HandleTypeDef* i2sHandle)
     }
 }
 
+void HAL_I2C_MspInit(I2C_HandleTypeDef* i2cHandle)
+{
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+    if(i2cHandle->Instance==I2C1)
+    {
+    /* USER CODE BEGIN I2C1_MspInit 0 */
+
+    /* USER CODE END I2C1_MspInit 0 */
+
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+    /**I2C1 GPIO Configuration
+    PB6     ------> I2C1_SCL
+    PB9     ------> I2C1_SDA
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_6|GPIO_PIN_9;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF4_I2C1;
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+    /* I2C1 clock enable */
+    __HAL_RCC_I2C1_CLK_ENABLE();
+    /* USER CODE BEGIN I2C1_MspInit 1 */
+
+    /* USER CODE END I2C1_MspInit 1 */
+    }
+}
+
 void HAL_CRC_MspInit(CRC_HandleTypeDef* hcrc)
 {
     if(hcrc->Instance==CRC) {
